48b63efcbd3f9c2cd6efb8b27082e1b5d9221da3
Shashi Kant <56475085+Shashikant17@users.noreply.github.com>
2021-09-28 21:50:39 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,7 @@
-welcome to lw cloud
-<br />
-
+<body bgcolor='white'>
 <pre>
 <?php
-
-print `ifconfig`;
-
+print("<b><font color=\"blue\">!! Hello !! This is Shashi Kant,\n\n! Welcome ! to My Web Server</font>\n\n");
+print (`/usr/sbin/ifconfig`);
 ?>
 </pre>
